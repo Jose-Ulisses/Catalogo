a04e539195ff867419467850ba797ae355ddf83d
jose ulisses <joseulissesbuenoo@gmail.com>
2025-04-25 12:20:41 -0300

    add categorias e produtos

---
+++ src/js/loadProducts.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const produtosContainer = document.getElementById("produtos-container");
+  
+    const totalProdutos = 10;
+    const produtos = [];
+
+    for(let i = 1; i <= totalProdutos; i++){
+        produtos.push({
+            nome: `produto ${i}`,
+            imagem: `../../imagens/casacos_jaquetas_masc/produto${i}.jpg`,
+            preco: "R$ 199,90",
+        });
+    }
+
+    produtos.forEach(produto => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+  
+      card.innerHTML = `
+        <img src="${produto.imagem}" alt="${produto.nome}">
+        <h3>${produto.nome}</h3>
+        <p>${produto.preco}</p>
+      `;
+  
+      produtosContainer.appendChild(card);
+    });
+  });
